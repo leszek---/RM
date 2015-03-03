0a477714b37edca47f518e84794ad297415cb83b
mczetyrbok <marcinczetyrbok@gmail.com>
2015-03-03 22:03:48 +0100

    wojna

--- RaportMaszynowy.Web/App_Start/BundleConfig.cs
+++ RaportMaszynowy.Web/App_Start/BundleConfig.cs
@@ -11,6 +11,17 @@ namespace RaportMaszynowy.Web
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                   "~/Scripts/angular.js",
+                   "~/Scripts/app/app.js",
+                   "~/Scripts/app/controllers.js"
+
+                   
+                   
+                   
+                   ));
+
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
---
+++ RaportMaszynowy.Web/Controllers/Api/ItemApiController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace RaportMaszynowy.Web.Controllers.Api
+{
+    public class ItemApiController : ApiController
+    {
+        // GET: api/Item
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Item/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Item
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Item/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Item/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
