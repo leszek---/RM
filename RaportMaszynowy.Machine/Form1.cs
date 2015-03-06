03c73915f7e230940c01167154d6b920e01f9e26
Marcin Czetyrbok <marcin.czetyrbok@partners.mbank.pl>
2015-03-06 12:11:38 +0100

    ItemSettingController

--- RaportMaszynowy.Machine/Form1.cs
+++ RaportMaszynowy.Machine/Form1.cs
@@ -1,21 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Windows.Forms;
 
 namespace RaportMaszynowy.Machine
 {
     public partial class Form1 : Form
     {
-
-
-
         public ShiftManager ShiftManager { get; set; }
+
         public ApiManager ApiManager { get; set; }
 
         public Form1()
@@ -33,10 +24,9 @@ namespace RaportMaszynowy.Machine
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-
-            //// tworzysz item 
-            //// ustawiasz mu settings 
-            //var task = new 
+            //// tworzysz item
+            //// ustawiasz mu settings
+            //var task = new
             //ApiManager.GetTaskSettings()
         }
     };
--- RaportMaszynowy.Web/Controllers/Api/ItemSettingsApiController.cs
+++ RaportMaszynowy.Web/Controllers/Api/ItemSettingsApiController.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 using FizzWare.NBuilder;
+using RaportManager.Domian;
 
 namespace RaportMaszynowy.Web.Controllers.Api
 {
     public class ItemSettingsApiController : ApiController
     {
 
-        public string GetActive()
+        public Settings GetActive()
         {
-            throw new NotImplementedException();
+            using (var context = new Model1())
+            {
+               var dbItem = context.Settings.SingleOrDefault(x => x.isActive == true);
+                return dbItem;
+            }
         }
 
 
