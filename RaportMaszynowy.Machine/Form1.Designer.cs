a5fe756af7d3d05641af4435f56e9e2b35387b5b
Marcin Czetyrbok <marcin.czetyrbok@partners.mbank.pl>
2015-03-06 12:18:15 +0100

    winformsy

--- RaportMaszynowy.Machine/Form1.Designer.cs
+++ RaportMaszynowy.Machine/Form1.Designer.cs
@@ -33,11 +33,14 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(6, 19);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(111, 23);
             this.button1.TabIndex = 0;
@@ -47,16 +50,17 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Location = new System.Drawing.Point(6, 48);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(111, 23);
             this.button2.TabIndex = 1;
             this.button2.Text = "Stop";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 70);
+            this.button3.Location = new System.Drawing.Point(6, 77);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(111, 23);
             this.button3.TabIndex = 2;
@@ -67,17 +71,38 @@
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(148, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(303, 181);
+            this.textBox1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(130, 181);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Akcje";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(411, 217);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(463, 213);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -87,6 +112,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
--- RaportMaszynowy.Machine/Form1.cs
+++ RaportMaszynowy.Machine/Form1.cs
@@ -5,8 +5,8 @@ namespace RaportMaszynowy.Machine
 {
     public partial class Form1 : Form
     {
+        public const int Interval = 100; 
         public ShiftManager ShiftManager { get; set; }
-
         public ApiManager ApiManager { get; set; }
 
         public Form1()
@@ -15,11 +15,13 @@ namespace RaportMaszynowy.Machine
             ShiftManager = new ShiftManager();
             ApiManager = new ApiManager();
             timer1.Start();
+            timer1.Interval = Interval;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             timer1.Start();
+            
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -28,6 +30,11 @@ namespace RaportMaszynowy.Machine
             //// ustawiasz mu settings
             //var task = new
             //ApiManager.GetTaskSettings()
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
         }
     };
 }
