179e4ea45cc121fd86e808c09452fc0616951a46
leszek--- <leszek.pawlega@gmail.com>
2015-03-06 14:07:19 +0100

    dodany API Manager, ale Bill jeden raczy wiedziec czy ma on sens

--- RaportMaszynowy.Machine/ApiManager.cs
+++ RaportMaszynowy.Machine/ApiManager.cs
@@ -1,26 +1,56 @@
 ﻿using System;
 using System.Security.Policy;
+using System.Net.Http;
+using RaportManager.Domian;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 
 namespace RaportMaszynowy.Machine
 {
     public class ApiManager
     {
+        HttpClient client = new HttpClient();
+        public string Url = "http://localhost:61471/";
+        
 
-        public static string Url = "costam";
-
-        public void  SendTask()
+        public void  SendItem(Item item)
         {
-            throw new NotImplementedException();
+            client.BaseAddress = new Uri(Url);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.PostAsJsonAsync("api/ItemApi", item);
+            
         }
 
-        public bool SendMachineStatus()
+        public bool SendMachineStatus(MachineError error)
         {
-            throw  new NotImplementedException();
+            client.BaseAddress = new Uri(Url);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.PostAsJsonAsync("api/ItemApi", error.Description).Result;
+            if (response.IsSuccessStatusCode)
+                return true;
+            else
+                return false;
         }
 
-        public bool GetTaskSettings()
+        public bool GetTaskSettings(int id)
         {
-            throw new NotImplementedException();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(Url);
+            string API = String.Concat("api/ItemApi/", id);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = client.GetAsync(API).Result;
+            
+            
+
+            if (response.IsSuccessStatusCode)
+            {
+                Item item = response.Content.ReadAsAsync<Item>().Result;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
     }
--- RaportMaszynowy.Machine/Program.cs
+++ RaportMaszynowy.Machine/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Net.Http;
 
 namespace RaportMaszynowy.Machine
 {
@@ -16,6 +17,13 @@ namespace RaportMaszynowy.Machine
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
+            HttpClient client = new HttpClient();
+            ApiManager ApiMgr = new ApiManager();
+            
+            
+            
+
         }
     }
 }
